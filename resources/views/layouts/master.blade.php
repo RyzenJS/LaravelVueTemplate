4614ab1f12cb4a8d29c4dfc27fb15006c0dc571a
RyzenJS <ryzenjs@gmail.com>
2019-12-11 00:21:09 +0800

    added logout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -110,12 +110,22 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </router-link>
         </li>
         <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-power-off"></i>
+             <a class="nav-link" href="{{ route('logout') }}"
+             onclick="event.preventDefault();
+                 document.getElementById('logout-form').submit();">
+                 <i class="nav-icon fas fa-power-off"></i>
               <p>
-                Logout
+                {{ __('Logout') }}
               </p>
-            </a>
+         
+             </a>
+
+         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+             @csrf
+         </form>
+
+
+
         </li>
         </ul>
       </nav>
